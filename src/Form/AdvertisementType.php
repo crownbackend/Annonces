7349f6dff87819a73e593ec92d00efb0181e6eab
crownbackend <crownbackend@gmail.com>
2018-09-09 01:45:24 +0200

    custome advertisement

--- src/Form/AdvertisementType.php
+++ src/Form/AdvertisementType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use App\Entity\Advertisement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +16,11 @@ class AdvertisementType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('price')
-            ->add('address')
-            ->add('zipCode')
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('price', NumberType::class)
+            ->add('address',TextType::class)
+            ->add('zipCode', NumberType::class)
             ->add('imageFile', FileType::class)
             ->add('imageFile2', FileType::class)
             ->add('imageFile3', FileType::class)
