fb293213d04dcbce0e25f6d3b10531afa53d532e
crownbackend <crownbackend@gmail.com>
2018-09-26 06:32:47 +0200

    pagination

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Advertisement;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,14 +27,27 @@ class BackController extends Controller
 
     /**
      * @Route("/annonces", name="back-advertisement", methods="GET")
+     * @param Request $request
      * @return Response
      */
-    public function advertisementAll(): Response {
+    public function advertisementAll(Request $request): Response {
+
+
+        $em    = $this->getDoctrine()->getManager();
+        $query = $em->getRepository(Advertisement::class)->findAll();
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            12
+        );
 
         $advertisements = $this->getDoctrine()->getRepository(Advertisement::class)->findAll();
 
         return $this->render('back/all-advertisement.html.twig', [
-            'advertisements' => $advertisements
+            'advertisements' => $advertisements,
+            'pagination' => $pagination
         ]);
 
     }
