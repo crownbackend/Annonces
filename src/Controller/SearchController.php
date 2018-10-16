d0182f9dcadfc82b9628157c6308dc1677959ee0
crownbackend <crownbackend@gmail.com>
2018-10-16 15:05:24 +0200

    search

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,11 +2,30 @@
 
 namespace App\Controller;
 
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/{_locale}")
+ */
 
 class SearchController extends Controller
 {
+    /**
+     * @Route({"fr": "/recherche/",
+     *         "en": "/search/",
+     *         "es": "/buscar/"}, name="search", methods="POST|GET")
+     * @param Request $request
+     * @return Response
+     */
+    public function searchAction(Request $request): Response
+    {
+
+
+        return $this->render('search/search.html.twig');
+
+    }
 
 }
