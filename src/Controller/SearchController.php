6be93b00e63ebf17b127998d651c1d3a1092d4bf
crownbackend <crownbackend@gmail.com>
2018-10-16 19:08:03 +0200

    end

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Advertisement;
 use App\Form\SearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,11 +21,19 @@ class SearchController extends Controller
      *         "es": "/buscar/"}, name="search", methods="POST|GET")
      * @param Request $request
      * @return Response
+     * @throws \Exception
      */
     public function searchAction(Request $request): Response
     {
         $form = $this->createForm(SearchType::class);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $content = $form->getData();
+            $value = $content['name'];
+            $search = $this->getDoctrine()->getRepository(Advertisement::class)->findBySearch();
+        }
 
         return $this->render('search/search.html.twig', [
             'form' => $form->createView()
--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Advertisement;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
