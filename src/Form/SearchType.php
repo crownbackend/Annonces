5f87a09c0ff5ce2de99756b9d395401dc7be9ce9
crownbackend <crownbackend@gmail.com>
2018-10-16 17:34:26 +0200

    form

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Advertisement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,27 +13,17 @@ class SearchType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name1')
-            ->add('name2', ChoiceType::class, [
-                'choices' => [
-                    'test' => 'lorem eugze',
-                    'test1' => 'lorem eugze',
-                ]
-            ])
-            ->add('name3')
-            ->add('name4', ChoiceType::class, [
-                'choices' => [
-                    'test' => 'lorem eugze',
-                    'test1' => 'lorem eugze',
-                ]
-            ])
+            ->add('name')
+            ->add('category')
+            ->add('address')
+            ->add('region')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => Advertisement::class
         ]);
     }
 }
