76a73a1bf4020cbb5fab3c5909b96a42201d3042
crownbackend <crownbackend@gmail.com>
2019-02-02 19:42:41 +0100

    test prod

--- src/Entity/Region.php
+++ src/Entity/Region.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\RegionRepository")
- * @ORM\Table(name="Region")
+ * @ORM\Table(name="region")
  */
 class Region
 {
